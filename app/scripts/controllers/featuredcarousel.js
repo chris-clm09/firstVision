07b519129065f94a10d62ea8daa286562bf4ca0e
Christopher Morgan <chris.morgan@domo.com>
2015-02-23 08:15:02 -0700

    inital carousel -- still broken

---
+++ app/scripts/controllers/featuredcarousel.js
@@ -0,0 +1,19 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name firstVisionApp.controller:FeaturedcarouselCtrl
+ * @description
+ * # FeaturedcarouselCtrl
+ * Controller of the firstVisionApp
+ */
+angular.module('firstVisionApp')
+  .controller('FeaturedCarouselCtrl', function ($scope) {
+
+    var ctrl = this;
+
+
+    this.test = 'Test Text';
+
+
+  });
