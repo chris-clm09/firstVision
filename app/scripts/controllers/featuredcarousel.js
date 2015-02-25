69868899689229f9ae66854c10ff270c15fe16f7
Christopher Morgan <chris.morgan@domo.com>
2015-02-24 20:21:10 -0700

    busted carousel

--- app/scripts/controllers/featuredcarousel.js
+++ app/scripts/controllers/featuredcarousel.js
@@ -7,13 +7,29 @@
  * # FeaturedcarouselCtrl
  * Controller of the firstVisionApp
  */
-angular.module('firstVisionApp')
-  .controller('FeaturedCarouselCtrl', function ($scope) {
+angular.module('firstVisionApp').controller('FeaturedCarouselCtrl', function ($scope) {
 
-    var ctrl = this;
+  var ctrl = this;
 
+  ctrl.featuredVideos = [
+    {
+      title: 'Dam Hand Stand',
+      url: '',
+      png: 'images/featured/feature1.png',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.'
+    },
+    {
+      title: 'O The Muscles',
+      url: '',
+      png: 'images/featured/feature2.png',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.'
+    },
+    {
+      title: 'Yo Ho Pirates',
+      url: '',
+      png: 'images/featured/feature3.png',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.'
+    }
+  ];
 
-    this.test = 'Test Text';
-
-
-  });
+});
